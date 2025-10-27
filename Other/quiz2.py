9cafc3f70cf269aaa6255c9e0ffddb0d6303243e
Veronika Sermeno Pon <pon45854@gmail.com>
2025-10-27 07:58:02 -0700

    made new folders

---
+++ Other/quiz2.py
@@ -0,0 +1,25 @@
+# ## Question 1
+# a = input("Enter first person's age: ")
+# b = input("Enter second person's age: ")
+# age_a = int(a)
+# age_b = int(b)
+# average = (age_a + age_b) / 2
+# print("Average age is", average)
+
+# string = "Average age is {}".format(average)
+# print(string)
+
+# ## Question 2
+# s = "TECHIN509"
+# reversed_str = s[::-1]
+# print(s[-1])
+# print(reversed_str)
+
+# ## Question 3
+# x = input("Enter a number: ")
+# print("Twice the number is:", int(x) * 2)
+
+## Question 4
+name = "Alice"
+age = 95
+print(f"{name} is {age} years-old")
